ae87701b83ccd6b38f41fd0589ec4b0193899df2
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-01-12 19:31:02 +0200

    foreach

--- loops.php
+++ loops.php
@@ -20,5 +20,19 @@
 	do{
 		echo $i.' ';
 		$i++;
-	}while($i<10);
+	}while($i<10);	
+	
+	echo '<br>';
+	
+	$y = array('Iosif', 'Giorgos', 'Kounas');
+	echo 'foreach: ';
+	foreach($y as $value){
+		echo $value.' ';
+	}
+	
+	echo '<br>';
+	echo 'foreach+keys: ';
+	foreach($y as $key => $value){
+		echo $key.'=>'.$value.' ';
+	}
 ?>
