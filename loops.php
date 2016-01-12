325e121029ac4104a581ee9ab9f6386cac9a7899
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-01-12 19:30:01 +0200

    do-while

--- loops.php
+++ loops.php
@@ -1,11 +1,24 @@
 <?php
+	echo 'for: ';
 	for($i=0;$i<10;$i++){
 		echo $i.' ';
 	}
+	
 	echo '<br>';
+	
+	echo 'while: ';
 	$i=0;
 	while($i<10){
 		echo $i.' ';
 		$i++;
 	}
+	
+	echo '<br>';
+	
+	echo 'do while: ';
+	$i=0;
+	do{
+		echo $i.' ';
+		$i++;
+	}while($i<10);
 ?>
