f11d0ceaae1f0be2622f2fce8bfe06123120f4f9
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-01-13 23:28:17 +0200

    switch

--- conditional-statements.php
+++ conditional-statements.php
@@ -13,4 +13,22 @@
 	echo "<br>";
 	
 	echo ($x==9)?"Equals":"Not Equals";
+	
+	echo "<br>";
+	
+	$fruit = "orange";
+	
+	switch($fruit){
+		case "apple":
+			echo "My favorite fruit is apple";
+			break;
+		case "orange":
+			echo "My favorite fruit is orange";
+			break;
+		case "melon":
+			echo "My favorite fruit is melon";
+			break;
+		default:
+			echo "My favorite fruit is banana";
+	}
 ?>
