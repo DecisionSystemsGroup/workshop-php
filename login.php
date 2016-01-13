93960da1bf7dd2819c31d3ab6a5efd0f932bbd4f
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-01-13 23:48:57 +0200

    add html code
    NO CSS, YO!

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Login</title>
+	</head>
+	<body>
+		<center>
+			<h2>Login</h2>
+			<fieldset>
+				<form action="login.php" method="post">
+					Username: <input type="text" name="username">
+					<br>
+					Password: <input type="password" name="password">
+					<br>
+					<input type="submit" value="Login">
+				</form>
+				<br>
+				If you don't have an account, register <a href="register.php">here</a>!
+			</fieldset>
+		</center>
+	</body>
+</html>
