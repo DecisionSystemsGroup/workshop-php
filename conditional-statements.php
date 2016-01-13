7ff5fe3d87f6f0626cbd319544d4dea6f2222e2b
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-01-14 00:27:19 +0200

    add php login handler

--- conditional-statements.php
+++ conditional-statements.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+	print_r($_SESSION);
 	$x = 3;
 	if($x>9){
 		echo "$x > 9";
--- login.php
+++ login.php
@@ -9,9 +9,9 @@
 			<h2>Login</h2>
 			<fieldset>
 				<form action="login.php" method="post">
-					Username: <input type="text" name="username">
+					Username: <input type="text" name="username" required>
 					<br>
-					Password: <input type="password" name="password">
+					Password: <input type="password" name="password" required>
 					<br>
 					<input type="submit" value="Login">
 				</form>
@@ -19,5 +19,34 @@
 				If you don't have an account, register <a href="register.php">here</a>!
 			</fieldset>
 		</center>
+
+<?php
+	session_start();
+	if(isset($_SESSION['username'])){
+		header("Location: index.php");
+		exit();
+	}
+	if($_POST){
+		include("mysqli_connect.php");
+		
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		$query = "SELECT email FROM users WHERE username='$username' AND password='$password'";
+		$result = mysqli_query($dbc,$query);
+		if($result->num_rows>0){
+			$row = $result->fetch_assoc();
+			session_start();
+			$_SESSION['email'] = $row['email'];
+			$_SESSION['username'] = $username;
+			header('Location: index.php');
+			exit();
+		}
+		else{
+			echo "<center style=\"color: red\">Wrong username or password!</center>";
+		}
+	}
+?>
+
 	</body>
 </html>
